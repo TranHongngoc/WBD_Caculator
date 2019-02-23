701336378a84adf0e963de8114b2a5740d9f29a4
hongngoc <hongngoctran0@gmail.com>
2019-02-23 15:33:20 +0700

    add color-picker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CaculatorComponent } from './caculator/caculator.component';
 import {FormsModule} from '@angular/forms';
+import { ColorPickComponent } from './color-pick/color-pick.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CaculatorComponent
+    CaculatorComponent,
+    ColorPickComponent
   ],
   imports: [
     BrowserModule,
